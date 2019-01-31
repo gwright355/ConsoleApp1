535929626ccc07fb26d32b609cc7886e36c21231
Glenn <Glenn@DESKTOP-76J73F3>
2019-01-31 18:03:50 -0500

    yes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("what is your name?");
+
+            Console.ReadKey(true);
+
+         try
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("Your name is " + input);
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter valid data");
+
+
+
+                Console.WriteLine("Press any key to exit the program and try again ...");
+
+                Console.ReadKey(true);
+            }
         }
     }
 }
